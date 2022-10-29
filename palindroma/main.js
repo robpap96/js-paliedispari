9aa5878823e063d37d0df7a95dfb7756d7f224a4
robpap96 <robertopapagni1996@outlook.it>
2022-10-29 17:06:49 +0200

    fatto funzione palindroma

--- palindroma/main.js
+++ palindroma/main.js
@@ -2,3 +2,24 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 "use strict";
+
+const parolaInserita = prompt("inserisci una parola");
+
+function palindroma(parolaInserita) {
+    let i = 0;
+    let j = parolaInserita.length - 1;
+    while(i < j){
+        if(parolaInserita[i] !== parolaInserita[j]){
+            debugger;
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+if(palindroma(parolaInserita)){
+    console.log("la parola inserita è palindroma");
+} else {
+    console.log("la parola inserita NON è palindroma");
+}
