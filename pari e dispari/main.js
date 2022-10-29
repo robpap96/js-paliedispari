2be6d9669e8fc035b11d2221d4fce1af341a35f1
robpap96 <robertopapagni1996@outlook.it>
2022-10-29 17:41:55 +0200

    finito pari o dispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -5,3 +5,26 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 "use strict";
+
+function getRndInteger (min, max) {
+    return Math.floor(Math.random() * (max-min +1)) + min;
+}
+function parChecker(num) {
+    if(num % 2 === 0){
+        return true;
+    }else {
+        return false;
+    }
+}
+const pariOdispari = prompt("scegli pari o dispari");
+const numeroInserito = Number(prompt("inserisci un numero da 1 a 5"));
+const numeroRandom = getRndInteger(1, 5);
+const tot = numeroInserito + numeroRandom;
+console.log(numeroInserito);
+console.log(numeroRandom);
+console.log(tot);
+if(parChecker(tot) && pariOdispari == "pari" || !parChecker(tot) && pariOdispari == "dispari") {
+    console.log("hai vinto");
+}else {
+    console.log("hai perso");
+}
